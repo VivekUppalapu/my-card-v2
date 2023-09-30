ea08ca9510805fb4cb0e70de0b08c78a835d914e
VivekUppalapu <rrvivek16@gmail.com>
2023-09-30 18:08:22 -0400

    Changes made

--- src/my-card-v2.js
+++ src/my-card-v2.js
@@ -28,66 +28,25 @@ class MyCardV2 extends LitElement {
   font-weight: bold;
   color: white;
 }
-.allbtns button
-{
-  font-size: 15px;
-  background-color: white;
-  color: teal;
-}
-.Detail
-{
-  font-size: 17px;
-  color: purple;
-  background-color: white;
-}
+
   `;
+  
 
   render() {
     return html`
       <div class="wrap">
   <div class="container">
     <div class="header">
-      <h3> THE MCU </h3>
+      <h3> THE MCU</h3>
     </div>
     <img class="image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp4884720.png&f=1&nofb=1&ipt=b86f47823ab6a8f5936b05270477294e1a27ee2b3b934a7789d434a72869fb8a&ipo=images">
     <div class="descrip">
-      <p> Growing up, I was a huge MCU fan. I watched all of the movies, some more than others, and I think that the picture that sums up how I feel about it is this one, which is the final battle in Endgame. I just felt pure joy seeing all these characters together.</p>
+      <p>Growing up, I was a huge MCU fan. I watched all of the movies, some more than others, and I think that the picture that sums up how I feel about it is this one, which is the final battle in Endgame. I just felt pure joy seeing all these characters together. </p>
     </div>
-    <button class="Detail">
-      Details</button>
+    </div>
   </div>
 </div>
-    <div class="allbtns">
-      <button class="duplicateBtn"> Duplicate Card</button>
-      <button class="changeBckgrnd"> Change Background </button>
-      <button class="ChangeTitle"> Change Title</button>
-      <button class="Delcrd"> Delete Card </button>
-    </div>
-<script>
-      document.querySelector('.duplicateBtn').addEventListener('click', (e) =>
-    {
-    const dupCard = document.querySelector('.wrap').cloneNode(true);
-      document.body.appendChild(dupCard);
-    });
-    document.querySelector('.changeBckgrnd').addEventListener('click', (e) => 
-    {
-      const bckColor = document.querySelector('.wrap');
-      bckColor.style.backgroundColor = 'black';
-    });
-    document.querySelector('.ChangeTitle').addEventListener('click',(e) =>
-    {
-      document.querySelector('h3').innerText = 'Marvel Cinematic Universe';
-    });
-    document.querySelector('.Delcrd').addEventListener('click', (e) =>
-    {
-      const allCrds = document.querySelectorAll(".wrap");
-      if(allCrds.length > 1)
-            {
-              const finalCard = allCrds[allCrds.length-1];
-              document.body.removeChild(finalCard);
-      }
-    });
-</script>
+    
     `;
   }
   
