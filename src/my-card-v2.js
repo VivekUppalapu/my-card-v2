eed20b75e8e0e4c584ebb814de338f86db6134b4
VivekUppalapu <rrvivek16@gmail.com>
2023-10-03 22:21:37 -0400

    Changes made to src

--- src/my-card-v2.js
+++ src/my-card-v2.js
@@ -1,8 +1,9 @@
 import { LitElement, html, css } from 'lit';
-const MCU = new URL('../assets/THE MCU.jpg', import.meta.url).href;
+const MCU = new URL('../assets/MCU.jpeg', import.meta.url).href;
 class MyCardV2 extends LitElement {
   static properties = {
-    header: { type: String },
+    title:{type: String},
+    descrip: {type: String}
   }
 
   static styles = css`
@@ -37,8 +38,10 @@ class MyCardV2 extends LitElement {
   constructor()
   {
     super();
-    this.name = "THE MCU";
-    this.description = "Growing up, I was a huge MCU fan. I watched all of the movies, some more than others, and I think that the picture that sums up how I feel about it is this one, which is the final battle in Endgame. I just felt pure joy seeing all these characters together.";
+    this.title = "THE MCU";
+    this.descrip = "Growing up, I was a huge MCU fan. I watched all of the movies, some more than others, and I think that the picture that sums up how I feel about it is this one, which is the final battle in Endgame. I just felt pure joy seeing all these characters together.";
+    
+  
   }
 
   render() {
@@ -46,11 +49,11 @@ class MyCardV2 extends LitElement {
       <div class="wrap">
   <div class="container">
     <div class="header">
-      <h3> ${this.name}</h3>
+      <h3> ${this.title}</h3>
     </div>
-    <img class="image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp4884720.png&f=1&nofb=1&ipt=b86f47823ab6a8f5936b05270477294e1a27ee2b3b934a7789d434a72869fb8a&ipo=images">
+    <img class="image" src="${MCU}">
     <div class="descrip">
-      <p>${this.description}</p>
+      <p>${this.descrip}</p>
       
     </div>
     </div>
